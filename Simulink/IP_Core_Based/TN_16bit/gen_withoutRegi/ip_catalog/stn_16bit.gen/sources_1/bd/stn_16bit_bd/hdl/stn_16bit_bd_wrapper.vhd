412acaf181facdab10b60d66511c9c9cd54e8dc1
MARUF AHMAD <m.ahmad2005@gmail.com>
2022-12-27 23:17:40 -0600

    IP_Core based design unwanted spike problem tested without registers

---
+++ Simulink/IP_Core_Based/TN_16bit/gen_withoutRegi/ip_catalog/stn_16bit.gen/sources_1/bd/stn_16bit_bd/hdl/stn_16bit_bd_wrapper.vhd
@@ -0,0 +1,40 @@
+--Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+----------------------------------------------------------------------------------
+--Tool Version: Vivado v.2021.1 (win64) Build 3247384 Thu Jun 10 19:36:33 MDT 2021
+--Date        : Mon Dec 26 00:25:05 2022
+--Host        : DESKTOP-1FT5C23 running 64-bit major release  (build 9200)
+--Command     : generate_target stn_16bit_bd_wrapper.bd
+--Design      : stn_16bit_bd_wrapper
+--Purpose     : IP block netlist
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity stn_16bit_bd_wrapper is
+  port (
+    clk : in STD_LOGIC;
+    input_t : in STD_LOGIC_VECTOR ( 15 downto 0 );
+    out_imag : out STD_LOGIC_VECTOR ( 15 downto 0 );
+    out_real : out STD_LOGIC_VECTOR ( 15 downto 0 )
+  );
+end stn_16bit_bd_wrapper;
+
+architecture STRUCTURE of stn_16bit_bd_wrapper is
+  component stn_16bit_bd is
+  port (
+    clk : in STD_LOGIC;
+    input_t : in STD_LOGIC_VECTOR ( 15 downto 0 );
+    out_imag : out STD_LOGIC_VECTOR ( 15 downto 0 );
+    out_real : out STD_LOGIC_VECTOR ( 15 downto 0 )
+  );
+  end component stn_16bit_bd;
+begin
+stn_16bit_bd_i: component stn_16bit_bd
+     port map (
+      clk => clk,
+      input_t(15 downto 0) => input_t(15 downto 0),
+      out_imag(15 downto 0) => out_imag(15 downto 0),
+      out_real(15 downto 0) => out_real(15 downto 0)
+    );
+end STRUCTURE;
---
+++ Simulink/IP_Core_Based/TN_32bit/STN_32bit/vivado/sysgen_STN/sysgen_STN.ip_user_files/sim_scripts/stn_32bit_0_2/xcelium/run.f
@@ -0,0 +1,64 @@
+-makelib xcelium_lib/xpm -sv \
+  "C:/Xilinx/Vivado/2021.1/data/ip/xpm/xpm_cdc/hdl/xpm_cdc.sv" \
+  "C:/Xilinx/Vivado/2021.1/data/ip/xpm/xpm_fifo/hdl/xpm_fifo.sv" \
+  "C:/Xilinx/Vivado/2021.1/data/ip/xpm/xpm_memory/hdl/xpm_memory.sv" \
+-endlib
+-makelib xcelium_lib/xpm \
+  "C:/Xilinx/Vivado/2021.1/data/ip/xpm/xpm_VCOMP.vhd" \
+-endlib
+-makelib xcelium_lib/xbip_utils_v3_0_10 \
+  "../../../ipstatic/hdl/xbip_utils_v3_0_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/c_reg_fd_v12_0_6 \
+  "../../../ipstatic/hdl/c_reg_fd_v12_0_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/xbip_dsp48_wrapper_v3_0_4 \
+  "../../../ipstatic/hdl/xbip_dsp48_wrapper_v3_0_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/xbip_pipe_v3_0_6 \
+  "../../../ipstatic/hdl/xbip_pipe_v3_0_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/xbip_dsp48_addsub_v3_0_6 \
+  "../../../ipstatic/hdl/xbip_dsp48_addsub_v3_0_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/xbip_addsub_v3_0_6 \
+  "../../../ipstatic/hdl/xbip_addsub_v3_0_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/c_addsub_v12_0_14 \
+  "../../../ipstatic/hdl/c_addsub_v12_0_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../../sysgen_STN.gen/sources_1/ip/stn_32bit_0_2/stn_32bit_c_addsub_v12_0_i0/sim/stn_32bit_c_addsub_v12_0_i0.vhd" \
+  "../../../../sysgen_STN.gen/sources_1/ip/stn_32bit_0_2/stn_32bit_c_addsub_v12_0_i1/sim/stn_32bit_c_addsub_v12_0_i1.vhd" \
+-endlib
+-makelib xcelium_lib/xbip_bram18k_v3_0_6 \
+  "../../../ipstatic/hdl/xbip_bram18k_v3_0_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/mult_gen_v12_0_17 \
+  "../../../ipstatic/hdl/mult_gen_v12_0_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/axi_utils_v2_0_6 \
+  "../../../ipstatic/hdl/axi_utils_v2_0_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/cordic_v6_0_17 \
+  "../../../ipstatic/hdl/cordic_v6_0_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../../sysgen_STN.gen/sources_1/ip/stn_32bit_0_2/stn_32bit_cordic_v6_0_i0/sim/stn_32bit_cordic_v6_0_i0.vhd" \
+  "../../../../sysgen_STN.gen/sources_1/ip/stn_32bit_0_2/stn_32bit_mult_gen_v12_0_i0/sim/stn_32bit_mult_gen_v12_0_i0.vhd" \
+  "../../../ipstatic/xil_defaultlib/hdl/conv_pkg.vhd" \
+  "../../../ipstatic/xil_defaultlib/hdl/synth_reg.vhd" \
+  "../../../ipstatic/xil_defaultlib/hdl/synth_reg_w_init.vhd" \
+  "../../../ipstatic/xil_defaultlib/hdl/srl17e.vhd" \
+  "../../../ipstatic/xil_defaultlib/hdl/srl33e.vhd" \
+  "../../../ipstatic/xil_defaultlib/hdl/synth_reg_reg.vhd" \
+  "../../../ipstatic/xil_defaultlib/hdl/single_reg_w_init.vhd" \
+  "../../../ipstatic/xil_defaultlib/hdl/xlclockdriver_rd.vhd" \
+  "../../../ipstatic/xil_defaultlib/hdl/stn_32bit_entity_declarations.vhd" \
+  "../../../ipstatic/xil_defaultlib/hdl/stn_32bit.vhd" \
+  "../../../../sysgen_STN.gen/sources_1/ip/stn_32bit_0_2/sim/stn_32bit_0.vhd" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  glbl.v
+-endlib
+
